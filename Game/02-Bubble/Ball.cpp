0a0f5e4de88a53523f4590830f7369650908fd9f
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-11-07 17:08:23 +0100

    Revert temporal changes to debug collisions

--- Game/02-Bubble/Ball.cpp
+++ Game/02-Bubble/Ball.cpp
@@ -27,8 +27,7 @@ namespace game
 			: m_map(i_collisionsManager)
 			, m_size(12)
 			, m_state(BallState::FollowingPlayer)
-			//, m_dirBall(CreateRandomStartDirection())
-			, m_dirBall(glm::normalize(glm::vec2(0,-1)))
+			, m_dirBall(CreateRandomStartDirection())
 			, m_speed(2.5f)
 			, m_player(i_player)
 			, m_currentMap(i_currentMap)
@@ -88,8 +87,7 @@ namespace game
 		void Ball::Reset()
 		{
 			m_state = BallState::FollowingPlayer;
-			//m_dirBall = CreateRandomStartDirection();
-			m_dirBall = glm::normalize(glm::vec2(0, 1));
+			m_dirBall = CreateRandomStartDirection();
 		}
 
 	}
